3c9dc81af9c598835e39e795a96f7ace424b8ff5
sllsboy <649718265@qq.com>
2022-08-15 17:38:32 +0800

    完成了登录和注册开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,5 @@
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+// 所有发起ajax的请求再发起之前先通过$.ajaxPrefilter进行预处理，因此这个函数可以拿到所有ajax请求的所有参数。
 $.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://www.liulongbin.top:3007' + options.url;
+
 })
